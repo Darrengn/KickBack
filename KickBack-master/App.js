59181afbd42f9afdebcf9267feb8ad64262fbcf9
Jeffrey Yang <cooljeffreyy@gmail.com>
2021-12-17 22:07:18 -0800

    initial commit

---
+++ KickBack-master/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+
+const AppStack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator 
+        headerMode="none"
+      >
+        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
+        <AppStack.Screen name="Signup" component={SignupScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
