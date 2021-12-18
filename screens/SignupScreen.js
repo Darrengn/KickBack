1faa9860d51bbfd610ea63882ae47a8582a0ec72
DarrenNg <ngwdarren@gmail.com>
2021-12-18 11:34:20 -0800

    Basic screen setup
    need to work on birthday and gender drop down.  also add sign in w/ google or facebook

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,14 +1,71 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput, Picker, ScrollView} from 'react-native';
 
 const SignupScreen = () => {
+    const [first, onChangeFirst] = React.useState('');
+    const [last, onChangeLast] = React.useState('');
+    const [gender, setGender] = React.useState("other");
+    const [email, onChangeEmail] = React.useState('');
+    const [vEmail, onChangeVEmail] = React.useState('');
+    const [username, onChangeUsername] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
+    const [vPassword, onChangeVPassword] = React.useState('');
+    
     return (
         <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <Button
-                title="Click Here"
+            <Text style = {{fontSize: 40}}>Create Account</Text>
+            <Button style ={styles.input}
+                title="Sign in with Google"
                 onPress={() => alert('Button Clicked!')}
             />
+            <Button style ={styles.input}
+                title="Continue with Facebook"
+                onPress={() => alert('Button Clicked!')}
+            />
+            <ScrollView>
+                <Text style = {{fontSize: 25}}>Or</Text>
+
+                <TextInput style={styles.input} onChangeText={onChangeFirst} value={first}>
+                    First Name
+                </TextInput>
+
+                <TextInput style={styles.input} onChangeText={onChangeLast} value={last}>
+                    Last Name
+                </TextInput>
+
+                <Text style = {{fontSize: 25}}>Birthday</Text>
+
+
+                
+                <View style = {styles.container}>
+                    <Text style = {{fontSize: 25}}>Gender</Text>
+                    <Picker
+                        selectedValue={gender}
+                        style={{ height: 20, width: 150 }}
+                        onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+                    >
+                        <Picker.Item label="Other" value="other" />
+                        <Picker.Item label="Male" value="male" />
+                        <Picker.Item label="Female" value="female" />
+                    </Picker>
+                </View>
+                
+                <TextInput style={styles.input} onChangeText={onChangeEmail} value={email}>
+                    Email Address
+                </TextInput>
+                <TextInput style={styles.input} onChangeText={onChangeVEmail} value={vEmail}>
+                    Verify Email Address
+                </TextInput>
+                <TextInput style={styles.input} onChangeText={onChangeUsername} value={username}>
+                    Username
+                </TextInput>
+                <TextInput style={styles.input} onChangeText={onChangePassword} value={password}>
+                    Password
+                </TextInput>
+                <TextInput style={styles.input} onChangeText={onChangeVPassword} value={vPassword}>
+                    Verify Password
+                </TextInput>
+            </ScrollView>
         </View>
     );
 };
@@ -21,4 +78,12 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+        height: 40,
+        width: 300,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        textAlign: 'center',
+      },
   });
