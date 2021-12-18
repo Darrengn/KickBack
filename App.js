e704d259f2598333a87e96014937a26c349cb507
Dylan Lee <dylanlee650@gmail.com>
2021-12-17 19:42:45 -0800

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const AppStack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppStack.Navigator 
+        headerMode="none"
+      >
+        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>Onboarding Screen</Text>
+            <Button
+                title="Click Here"
+                onPress={() => navigation.navigate("Login")}
+            />
+        </View>
+    );
+}
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
