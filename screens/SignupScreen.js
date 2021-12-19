c62ad32908de1c03fb0dab2f4bd509f02a978c86
DarrenNg <ngwdarren@gmail.com>
2021-12-18 16:28:11 -0800

    Added header bar and back button
    still need to add birthday and gender drop downs

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, TextInput, Picker, ScrollView} from 'react-native';
 
-const SignupScreen = () => {
+const SignupScreen = ({navigation}) => {
     const [first, onChangeFirst] = React.useState('');
     const [last, onChangeLast] = React.useState('');
     const [gender, setGender] = React.useState("other");
@@ -12,60 +12,72 @@ const SignupScreen = () => {
     const [vPassword, onChangeVPassword] = React.useState('');
     
     return (
-        <View style={styles.container}>
-            <Text style = {{fontSize: 40}}>Create Account</Text>
-            <Button style ={styles.input}
-                title="Sign in with Google"
-                onPress={() => alert('Button Clicked!')}
-            />
-            <Button style ={styles.input}
-                title="Continue with Facebook"
-                onPress={() => alert('Button Clicked!')}
-            />
-            <ScrollView>
+        <View style={{flex:1}}>
+            <View style = {{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
+                <Button
+                    title="  Back"
+                    onPress={() => navigation.navigate("Onboarding")}
+                />
+            </View>
+            <View style = {[styles.container, {flex: 12}]}>
+                
+                <Text style = {{fontSize: 40}}>Create Account</Text>
+                <Button style ={styles.input}
+                    title="Sign in with Google"
+                    onPress={() => alert('sign in with google')}
+                />
+                <Button style ={styles.input}
+                    title="Continue with Facebook"
+                    onPress={() => alert('sign in with facebook')}
+                />
                 <Text style = {{fontSize: 25}}>Or</Text>
+                <ScrollView>
+                    <View style = {styles.container}>
+                        
 
-                <TextInput style={styles.input} onChangeText={onChangeFirst} value={first}>
-                    First Name
-                </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangeFirst} value={first}>
+                            First Name
+                        </TextInput>
 
-                <TextInput style={styles.input} onChangeText={onChangeLast} value={last}>
-                    Last Name
-                </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangeLast} value={last}>
+                            Last Name
+                        </TextInput>
 
-                <Text style = {{fontSize: 25}}>Birthday</Text>
+                        <Text style = {{fontSize: 25}}>Birthday</Text>
 
 
-                
-                <View style = {styles.container}>
-                    <Text style = {{fontSize: 25}}>Gender</Text>
-                    <Picker
-                        selectedValue={gender}
-                        style={{ height: 20, width: 150 }}
-                        onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
-                    >
-                        <Picker.Item label="Other" value="other" />
-                        <Picker.Item label="Male" value="male" />
-                        <Picker.Item label="Female" value="female" />
-                    </Picker>
-                </View>
-                
-                <TextInput style={styles.input} onChangeText={onChangeEmail} value={email}>
-                    Email Address
-                </TextInput>
-                <TextInput style={styles.input} onChangeText={onChangeVEmail} value={vEmail}>
-                    Verify Email Address
-                </TextInput>
-                <TextInput style={styles.input} onChangeText={onChangeUsername} value={username}>
-                    Username
-                </TextInput>
-                <TextInput style={styles.input} onChangeText={onChangePassword} value={password}>
-                    Password
-                </TextInput>
-                <TextInput style={styles.input} onChangeText={onChangeVPassword} value={vPassword}>
-                    Verify Password
-                </TextInput>
-            </ScrollView>
+                        
+                        <View style = {styles.container}>
+                            <Text style = {{fontSize: 25}}>Gender</Text>
+                            <Picker
+                                selectedValue={gender}
+                                style={{ height: 20, width: 150 }}
+                                onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+                            >
+                                <Picker.Item label="Other" value="other" />
+                                <Picker.Item label="Male" value="male" />
+                                <Picker.Item label="Female" value="female" />
+                            </Picker>
+                        </View>
+                        
+                        <TextInput style={styles.input} onChangeText={onChangeEmail} value={email}>
+                            Email Address
+                        </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangeVEmail} value={vEmail}>
+                            Verify Email Address
+                        </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangeUsername} value={username}>
+                            Username
+                        </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangePassword} value={password}>
+                            Password
+                        </TextInput>
+                        <TextInput style={styles.input} onChangeText={onChangeVPassword} value={vPassword}>
+                            Verify Password
+                        </TextInput>
+                    </View>
+                </ScrollView>
+            </View>
         </View>
     );
 };
