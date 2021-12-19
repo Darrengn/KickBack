c9be8b4c5efb1a97b9d9d90059bb4e7f91e1279c
DarrenNg <ngwdarren@gmail.com>
2021-12-18 17:01:10 -0800

    Added basic login page
    needs sign in with google / facebook

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
+    const [email, onChangeEmail] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
     return (
-        <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <Button
-                title="Click Here"
-                onPress={() => alert('Button Clicked!')}
-            />
+        <View style={{flex: 1}}>
+            <View style = {{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
+                <Button
+                    title="  Back"
+                    onPress={() => navigation.navigate("Onboarding")}
+                />
+            </View>
+            <View style = {[styles.container,{flex:12}]}>
+                <View style = {[styles.container, {flex: 10, justifyContent: 'flex-end'}]}>
+                    <Text style = {{fontSize: 40}}>Kickback</Text>
+                    <TextInput style={styles.input} onChangeText={onChangeEmail} value={email}>
+                        Email Address
+                    </TextInput>
+                    <TextInput style={styles.input} onChangeText={onChangePassword} value={password}>
+                        Password
+                    </TextInput>
+                    <Text style = {{fontSize: 25}}>OR</Text>
+                </View>
+                <View style = {{flex: 2,}}>
+                    <Button style ={styles.input}
+                        title="Sign in with Google"
+                        onPress={() => alert('sign in with google')}
+                    />
+                    <Button style ={styles.input}
+                    title="Continue with Facebook"
+                    onPress={() => alert('sign in with facebook')}
+                    />
+                </View>
+                <View style = {{flex: 1, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center'}}>
+                    <Button style ={styles.input}
+                        title="Login"
+                        color='white'
+                        onPress={() => alert('Login')}
+                    />
+                </View>
+                <View style = {{flex: 8}}></View>
+            </View>
         </View>
     );
 };
@@ -21,4 +54,12 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+        height: 40,
+        width: 300,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        textAlign: 'center',
+      },
   });
