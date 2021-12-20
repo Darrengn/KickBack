cedae5ed4036dc12b331b073961a808355db76d4
DarrenNg <ngwdarren@gmail.com>
2021-12-19 17:48:05 -0800

    changed buttons to TouchableWtihoutFeedback

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback } from 'react-native';
 
 const OnboardingScreen = ({navigation}) => {
     const [email, onChangeEmail] = React.useState('');
@@ -12,21 +12,19 @@ const OnboardingScreen = ({navigation}) => {
             <View style = {[styles.container,{flex:12}]}>
                 <View style = {[styles.container, {flex: 5, justifyContent: 'flex-end'}]}>
                     <View style = {{flex:1}}></View>
-                    <View style = {{flex: 1, borderWidth: 1, width:300, justifyContent:'center'}}>
-                    <Button style ={styles.input}
-                        title="Login"
-                        onPress={() => navigation.navigate("Login")}
-                    />
-                    </View>
+                    <TouchableWithoutFeedback onPress={() => navigation.navigate("Login")}>
+                        <View style = {{flex: 1, borderWidth: 1, width:300, justifyContent:'center', alignItems:'center'}}>
+                            <Text style = {{fontSize: 25}}> Login </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+
                     <View style = {{flex:1}}></View>
-                    <View style = {{flex: 1, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center'}}>
-                    <Button style ={[styles.input, {textAlign: 'center'}]}
-                        title="Create Account"
-                        color='white'
-                        onPress={() => navigation.navigate("Signup")}
-                    />
+                    <TouchableWithoutFeedback onPress={() => navigation.navigate("Signup")}>
+                        <View style = {{flex: 1, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center', alignItems:'center'}}>
+                            <Text style = {{fontSize: 25, color: 'white'}}> Create Account </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
                     <View style = {{flex:1}}></View>
-                    </View>
                 </View>
                 <View style = {{flex: 2,}}>
                 </View>
