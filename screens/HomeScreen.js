4b5506cb7f6e2aea5b6bbf4bc11c47c874a1fd8f
DarrenNg <ngwdarren@gmail.com>
2021-12-20 15:56:54 -0800

    Added boxes for image and description, also added scrolling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TextInput, TouchableWithoutFeedback, Touchable } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, ScrollView } from 'react-native';
 
 const HomeScreen = ({navigation}) => {
     const [search, onChangeSearch] = React.useState('');
     return (
         <View style={{flex: 1}}>
-            <View style = {{flex: '7&', alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
+            <View style = {{flex: '7%', alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
                 
             </View>
 
@@ -25,56 +25,157 @@ const HomeScreen = ({navigation}) => {
                         placeholder='Search'    
                     />
                 </View>
+                <View style = {{flex: 12}}>
+                    <ScrollView 
+                        showsVerticalScrollIndicator ={false}                      
+                    >
+                        <View style = {[styles.container,{flex:6, justifyContent: 'flex-start', width: 350}]}>
+                            <Text style = {{fontSize: 14, fontWeight: 'bold', alignSelf:'flex-start'}}>Upcoming Events</Text>
+                            <TouchableWithoutFeedback onPress={() => alert('Go to Event 1')}>
+                                <View style = {styles.event}>
+                                    <View style ={{flex:'22.5%',borderWidth:1}}>
+                                        <Text>Image here</Text>
+                                    </View>
+                                    <View style = {{flex:'77.5%'}}>
+                                        <Text style = {{fontWeight:'bold'}}>Event 1</Text>
+                                        <Text>Description</Text>
+                                        <Text>Date</Text>
+                                        <Text style = {{fontSize: 10}}>Friends going</Text>
+                                    </View>
+                                </View>
+                            </TouchableWithoutFeedback>
 
-                <View style = {[styles.container,{flex:6, justifyContent: 'flex-start', width: 350}]}>
-                    <Text style = {{fontSize: 14, fontWeight: 'bold', alignSelf:'flex-start'}}>Upcoming Events</Text>
-                    
-                    <View style = {styles.event}>
-
-                    </View>
-
-                    <View style = {styles.event}>
+                            <TouchableWithoutFeedback onPress={() => alert('Go to Event 2')}>
+                                <View style = {styles.event}>
+                                    <View style ={{flex:'22.5%',borderWidth:1}}>
+                                        <Text>Image here</Text>
+                                    </View>
+                                    <View style = {{flex:'77.5%'}}>
+                                        <Text style = {{fontWeight:'bold'}}>Event 2</Text>
+                                        <Text>Description</Text>
+                                        <Text>Date</Text>
+                                        <Text style = {{fontSize: 10}}>Friends going</Text>
+                                    </View>
+                                </View>
+                            </TouchableWithoutFeedback>    
+                            
+                            <TouchableWithoutFeedback onPress={() => alert('Go to Event 3')}>
+                                <View style = {styles.event}>
+                                    <View style ={{flex:'22.5%',borderWidth:1}}>
+                                        <Text>Image here</Text>
+                                    </View>
+                                    <View style = {{flex:'77.5%'}}>
+                                        <Text style = {{fontWeight:'bold'}}>Event 3</Text>
+                                        <Text>Description</Text>
+                                        <Text>Date</Text>
+                                        <Text style = {{fontSize: 10}}>Friends going</Text>
+                                    </View>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        </View>
                         
-                    </View>
+                        <View style = {[styles.container,{flex:6, justifyContent: 'flex-start', width: 350}]}>
+                            <Text style = {{fontSize: 14, fontWeight: 'bold', alignSelf:'flex-start'}}>Suggested Events</Text>
+                            <View style = {{width: 400, alignItems:'center'}}>
+                                
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 4')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 4</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
 
-                    <View style = {styles.event}>
-                        
-                    </View>
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 5')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 5</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10,}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 6')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 6</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 7')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 7</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 8')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 8</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                
+                                    <TouchableWithoutFeedback onPress={() => alert('Go to Event 9')}>
+                                        <View style = {styles.event}>
+                                            <View style ={{flex:'22.5%',borderWidth:1}}>
+                                                <Text>Image here</Text>
+                                            </View>
+                                            <View style = {{flex:'77.5%'}}>
+                                                <Text style = {{fontWeight:'bold'}}>Event 9</Text>
+                                                <Text>Description</Text>
+                                                <Text>Date</Text>
+                                                <Text style = {{fontSize: 10}}>Friends going</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
 
-                <View style = {[styles.container,{flex:6, justifyContent: 'flex-start', width: 350}]}>
-                    <Text style = {{fontSize: 14, fontWeight: 'bold', alignSelf:'flex-start'}}>Suggested Events</Text>
-                    <TouchableWithoutFeedback>
-                    <View style = {styles.event}>
-                        
-                    </View>
-                    </TouchableWithoutFeedback>
-
-                    <View style = {styles.event}>
-                        
-                    </View>
-
-                    <View style = {styles.event}>
-                        
-                    </View>
-                </View>
-
-                <TouchableWithoutFeedback onPress={() => alert('Login')}>
-                    <View style = {[styles.event, {flex: 1, flexDirection:'row', justifyContent: 'center', alignItems:'center', borderRadius:10}]}>
-                        <Text style = {{fontSize: 20, fontWeight: 'bold'}}>See More</Text>
-                    </View>
-                </TouchableWithoutFeedback>
-
-                <View style= {{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignContent:'space-between', width: 400}}>
-                    <TouchableWithoutFeedback>
+                <View style= {{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems:'center', width: 400, borderTopWidth: 2}}>
+                    <TouchableWithoutFeedback onPress={() => alert('Home')}>
                         <Text>Home</Text>
                     </TouchableWithoutFeedback>
 
-                    <TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback onPress={() => alert('Host')}>
                         <Text>Host</Text>
                     </TouchableWithoutFeedback>
 
-                    <TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback onPress={() => alert('Add Friend')}>
                         <Text>Add Friend</Text>
                     </TouchableWithoutFeedback>
                 </View>
@@ -101,9 +202,10 @@ const styles = StyleSheet.create({
         textAlign: 'center',
       },
     event: {
-        height: '25%',
+        height: 70,
         width: 350,
         margin: 6,
         borderWidth: 2,
+        flexDirection:'row'
     },
   });
