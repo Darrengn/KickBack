274daea47de6b4f73b425ba3fdfce0aff3ec4101
DarrenNg <ngwdarren@gmail.com>
2021-12-20 13:51:17 -0800

    Fix keyboard and changed button
    Made birthday a text box and fixed keyboardAvoidingView

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -4,6 +4,7 @@ import { View, Text, Button, StyleSheet, TextInput, Picker, ScrollView, Keyboard
 const SignupScreen = ({navigation}) => {
     const [first, onChangeFirst] = React.useState('');
     const [last, onChangeLast] = React.useState('');
+    const [birthday, onChangeBirthday] = React.useState('');
     const [gender, setGender] = React.useState("other");
     const [email, onChangeEmail] = React.useState('');
     const [vEmail, onChangeVEmail] = React.useState('');
@@ -12,16 +13,20 @@ const SignupScreen = ({navigation}) => {
     const [vPassword, onChangeVPassword] = React.useState('');
     
     return (
-        <View style={{flex:1}}>
-            <View style = {{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
+        <View style={{flex:1,}}>
+            <View style = {{flex: '7%', alignItems:'flex-start', justifyContent:'flex-end'}}>
                 <Button
+                    style = {{}}
                     title="  Back"
                     onPress={() => navigation.navigate("Onboarding")} // needs to change to touchable and needs symbol
                 />
             </View>
-            <View style = {[styles.container, {flex: 12}]}>
-                
-                <Text style = {{fontSize: 40}}>Create Account</Text>
+
+            <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                    style = {[styles.container, {flex: '93%', justifyContent:'flex-start'}]}
+            >
+                <Text style = {{fontSize: 40, textAlign:'center', alignSelf:'center'}}>Create Account</Text>
                 <Button style ={styles.input}
                     title="Sign in with Google"
                     onPress={() => alert('sign in with google')}
@@ -30,30 +35,38 @@ const SignupScreen = ({navigation}) => {
                     title="Continue with Facebook"
                     onPress={() => alert('sign in with facebook')}
                 />
-                <Text style = {{fontSize: 25}}>Or</Text>
+                <Text style = {{fontSize: 25}}>
+                    Or
+                </Text>
                 <ScrollView>
-                    <KeyboardAvoidingView style = {[styles.container, {flex: 5}]}>
+                
 
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeFirst} 
-                            value={first}
-                            placeholder='First Name'
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangeFirst} 
+                        value={first}
+                        placeholder='First Name'
+                    />
+
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangeLast} 
+                        value={last}
+                        placeholder='Last Name'
+                    />
+                    <View style = {[styles.input, {flexDirection:'row', justifyContent:'space-between'}]}>
+                        <Text style = {{fontSize: 14, }}>Birthday</Text>
+                        <TextInput
+                            style = {{textAlignVertical:'center'}}
+                            onChangeText={onChangeBirthday} 
+                            value={birthday}
+                            placeholder='MM/DD/YYYY'
                         />
-
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeLast} 
-                            value={last}
-                            placeholder='Last Name'
-                        />
-
-                        <Text style = {{fontSize: 20}}>Birthday</Text> 
-                    </KeyboardAvoidingView>
 
                         
-                    <View style = {[styles.input, {flexDirection: 'row', borderWidth: 1,}]}>
-                        <Text style = {{fontSize: 18}}>Gender</Text>
+                    </View>
+                    <View style = {[styles.input, {flexDirection: 'row', borderWidth: 2, justifyContent:'flex-start'}]}>
+                        <Text style = {{fontSize: 14, textAlignVertical:'center'}}>Gender</Text>
                         <Picker // Still needs work
                             selectedValue={gender}
                             style={{ height: 10, width: 150, alignSelf:'flex-end' }}
@@ -65,52 +78,50 @@ const SignupScreen = ({navigation}) => {
                         </Picker>
                     </View>
 
-                    <KeyboardAvoidingView style = {styles.container}>
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeEmail} 
-                            value={email}
-                            placeholder='Email Address'
-                        />
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeVEmail} 
-                            value={vEmail}
-                            placeholder='Verify Email Address'
-                        />
-                        <TextInput style=
-                            {styles.input} 
-                            onChangeText={onChangeUsername} 
-                            value={username}
-                            placeholder='Username'
-                        />
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangePassword} 
-                            value={password}
-                            placeholder='Password'
-                            secureTextEntry
-                        />
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeVPassword} 
-                            value={vPassword}
-                            placeholder='Verify Password'
-                            secureTextEntry
-                        />
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangeEmail} 
+                        value={email}
+                        placeholder='Email Address'
+                    />
                         
-                        
-                    
-                    </KeyboardAvoidingView>
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangeVEmail} 
+                        value={vEmail}
+                        placeholder='Verify Email Address'
+                    />
+                    <TextInput style=
+                        {styles.input} 
+                        onChangeText={onChangeUsername} 
+                        value={username}
+                        placeholder='Username'
+                    />
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangePassword} 
+                        value={password}
+                        placeholder='Password'
+                        secureTextEntry
+                    />
+                    <TextInput 
+                        style={styles.input} 
+                        onChangeText={onChangeVPassword} 
+                        value={vPassword}
+                        placeholder='Verify Password'
+                        secureTextEntry
+                    />
+
                     <TouchableWithoutFeedback onPress={() => alert('Create Account')}>
-                    <View style = {{backgroundColor: 'black', width: 150, height:50, alignSelf: 
-                        'center', alignItems:'center', justifyContent:'center'}}>
-                        
-                        <Text style= {{color: 'white', fontSize: 20 }}>Create Account</Text>
-                    </View>
+                        <View style = {{backgroundColor: 'black', width: 150, height:50, alignSelf: 
+                            'center', alignItems:'center', justifyContent:'center', borderRadius:'15'}}>
+                            
+                            <Text style= {{color: 'white', fontSize: 20 }}>Create Account</Text>
+                        </View>
                     </TouchableWithoutFeedback>
+
                 </ScrollView>
-            </View>
+            </KeyboardAvoidingView>
         </View>
     );
 };
@@ -127,8 +138,8 @@ const styles = StyleSheet.create({
         height: 40,
         width: 300,
         margin: 12,
-        borderWidth: 1,
+        borderWidth: 2,
         padding: 10,
-       
+        fontSize:14
       },
   });
