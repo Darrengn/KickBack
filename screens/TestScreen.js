da9e490a90abf98ea7710043827c02def93590dd
DarrenNg <ngwdarren@gmail.com>
2022-02-17 14:52:37 -0800

    backend interface

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Button, SafeAreaView, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-function httpGet(url){
+/*function httpGet(url){
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, true );
+  xmlHttp.open("GET", url, true);
   xmlHttp.setRequestHeader("AuthToken", "admin");
   xmlHttp.send(null);
   return xmlHttp.responseText;
 }
 
+function httpPost(url){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", url, true);
+  xmlHttp.setRequestHeader("AuthToken", "admin");
+  console.log(xmlHttp.send(event));
+}*/
+
+
+
+async function getEvent() {
+  try {
+    let response = await fetch('34.201.126.106:9090/events/', {
+      method: 'GET',
+      headers: {
+        "AuthToken": 'admin',
+        'Content-Type': 'application/json',
+      },
+    });
+    let responseJson = await response.json();
+    console.log(responseJSon);
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const event = {"name": "Event 1", "owner": "test"}
+
 const TestScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
-          <TouchableOpacity onPress={() => navigation.navigate("Onboarding")}
+            <SafeAreaView>
+                <StatusBar barStyle="dark-content"/>
+            </SafeAreaView>
+
+            <TouchableOpacity onPress={() => navigation.navigate("Onboarding")}
                 style = {{flex: '4%', alignItems: 'flex-end', justifyContent: 'flex-start', flexDirection:'row',}}>
                 <View style = {{width: 20}}></View>
                 <Icon name="arrow-back-outline" size={30} color='black' />
             </TouchableOpacity>
-            <Text>{httpGet("34.201.126.106:9090/event/1")}</Text>
+
+            <Button
+                title="create event"
+                onPress={() => getEvent()}
+            /> 
+
         </View>
     );
 }
