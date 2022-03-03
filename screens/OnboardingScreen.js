649dcf4b4ab116e44adb725306fe9d1fd32e712b
DarrenNg <ngwdarren@gmail.com>
2022-03-02 17:50:40 -0800

    initial edits to onboarding screen

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,61 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, SafeAreaView, StatusBar } from 'react-native';
 
 const OnboardingScreen = ({navigation}) => {
-    const [email, onChangeEmail] = React.useState('');
-    const [password, onChangePassword] = React.useState('');
-    return (
-        <View style={{flex: 1}}>
-            <View style = {{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', backgroundColor: 'black'}}>
-                
-            </View>
-            <View style = {[styles.container,{flex:12}]}>
-                <View style = {[styles.container, {flex: 5, justifyContent: 'flex-end'}]}>
-                    <View style = {{flex:1}}></View>
-                    <TouchableWithoutFeedback onPress={() => navigation.navigate("Login")}>
-                        <View style = {{flex: 1, borderWidth: 1, width:300, justifyContent:'center', alignItems:'center'}}>
-                            <Text style = {{fontSize: 25}}> Login </Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+	const [email, onChangeEmail] = React.useState('');
+		const [password, onChangePassword] = React.useState('');
+		return (
+			<View style={{flex: 1, backgroundColor: 'black'}}>
+				<SafeAreaView>
+					<StatusBar barStyle="light-content"/>
+				</SafeAreaView>
 
-                    <View style = {{flex:1}}></View>
-                    <TouchableWithoutFeedback onPress={() => navigation.navigate("Signup")}>
-                        <View style = {{flex: 1, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center', alignItems:'center'}}>
-                            <Text style = {{fontSize: 25, color: 'white'}}> Create Account </Text>
-                        </View>
-                    </TouchableWithoutFeedback>
-                    <View style = {{flex:1}}></View>
-                </View>
-                <View style = {{flex: 2,}}>
-                </View>
-                <View style = {{flex: 8, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center'}}>
-                    <Button style ={styles.input}
-                        title="Logo here"
-                        color='white'
-                        onPress={() => alert('Login')}
-                    />
-                </View>
-                <View style = {{flex: 3}}></View>
-            </View>
-        </View>
-    );
+				<View style = {styles.container}>
+					
+					<View style = {{flex:'10'}}></View>
+					
+					<View style = {{flex: '35%', borderWidth: 1, backgroundColor: 'white', width:300, justifyContent:'center'}}>
+						<Button style ={styles.input}
+							title="Logo here"
+							color='black'
+							onPress={() => alert('Login')}
+						/>
+					</View>
+
+					<View style = {{flex: '10%', justifyContent: 'center'}}>
+						<Text style = {{fontSize: 40, color: 'white'}}>Kickback</Text>
+					</View>
+
+					<View style = {{flex: '5%',}}>
+						<Text style = {{fontSize: 14, color: '#79abe0'}}>LET'S GET THE PARTY STARTED</Text>
+					</View>
+
+					<View style = {{flex:'2%'}}></View>
+
+					<TouchableWithoutFeedback onPress={() => navigation.navigate("Login")}>
+						<View style = {{flex: '5%', borderWidth: 2, borderRadius: 50, width:300, 
+							justifyContent:'center', alignItems:'center', backgroundColor:'#96c3f2'}}>
+							
+							<Text style = {{fontSize: 20, fontWeight:'bold'}}> Login </Text>
+						</View>
+					</TouchableWithoutFeedback>
+
+					<View style = {{flex:'5%'}}></View>
+					
+					<TouchableWithoutFeedback onPress={() => navigation.navigate("Signup")}>
+						<View style = {{flex: '5%', borderWidth: 2, borderRadius: 50, width:300, 
+							justifyContent:'center', alignItems:'center',borderColor:'#96c3f2'}}>
+							<Text style = {{fontSize: 20, color: 'white',fontWeight:'bold'}}> Create Account </Text>
+						</View>
+					</TouchableWithoutFeedback>
+										
+					<View style = {{flex:'23'}}></View>
+				</View>
+			</View>
+		);
 }
 
 export default OnboardingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    input: {
-        height: 40,
-        width: 300,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-        textAlign: 'center',
-      },
-  });
-  
+		container: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+		input: {
+				height: 40,
+				width: 300,
+				margin: 12,
+				borderWidth: 1,
+				padding: 10,
+				textAlign: 'center',
+			},
+	});
+	
