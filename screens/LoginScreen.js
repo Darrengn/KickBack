934510bd691c9e11aa43841097012d8e44962980
DarrenNg <ngwdarren@gmail.com>
2022-03-24 17:40:44 -0700

    New login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,78 +6,90 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 
 const LoginScreen = ({navigation}) => {
-    const [email, onChangeEmail] = React.useState('');
+    const [number, onNumber] = React.useState('');
     const [password, onChangePassword] = React.useState('');
 
-    const handleLogin = () => {
-        auth
-          .signInWithEmailAndPassword(email, password)
-          .then(userCredentials => {
-            const user = userCredentials.user;
-            console.log('Logged in with ' + user.email);
-        })
-        .catch(error => alert(error.message))
-    }
-
     return (
-        <View style={{flex: 1, backgroundColor:'#EDF6F9'}}>
+        <View style={[styles.container,{flex: 1, backgroundColor:'black'}]}>
             
             <SafeAreaView>
-                <StatusBar barStyle="dark-content"/>
+                <StatusBar barStyle="light-content"/>
             </SafeAreaView>
 
-            <TouchableOpacity onPress={() => navigation.navigate("Onboarding")}
-                style = {{flex: '4%', alignItems: 'flex-end', justifyContent: 'flex-start', flexDirection:'row',}}>
-                <View style = {{width: 20}}></View>
-                <Icon name="arrow-back-outline" size={30} color='black' />
-            </TouchableOpacity>
+            <View style = {{flex: '5%', alignItems: 'flex-end', justifyContent: 'flex-start', flexDirection:'row',}}>
+                <View style = {{flex: '5%'}}></View>
 
-            <View style = {{flex: '96%'}}>
-                <View style = {[styles.container,{flex:12,}]}>
-                    <View style = {[styles.container, {flex: 10, justifyContent: 'flex-end'}]}>
-                        <Text style = {{fontSize: 40,}}>Kickback</Text>
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangeEmail} 
-                            value={email}
-                            placeholder='Email'
-                        />
-                        <TextInput 
-                            style={styles.input} 
-                            onChangeText={onChangePassword} 
-                            value={password}
-                            placeholder='Password'
-                            secureTextEntry
-                        />
-                        <Text style = {{fontSize: 25}}>OR</Text>
-                    </View>
-                    <View style = {{flex: 2, padding: 10}}>
-                        <Button style ={styles.input}
-                            title="Sign in with Google"
-                            onPress={() => alert('sign in with google')}
-                        />
-                        <Button style ={styles.input}
-                        title="Continue with Facebook"
-                        onPress={() => alert('sign in with facebook')}
-                        />
-                    </View>
-                    <View style = {{flex: 1}}>
-                        <TouchableOpacity onPress={handleLogin}>
-                            <View style = {{flex: 1, borderWidth: 1, backgroundColor: 'black', width:300, justifyContent:'center', 
-                                alignItems:'center', borderRadius:10}}>
-                                
-                                <Text style={{color: 'white', fontSize: 20}}>Login</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    <View style= {[styles.input,{borderWidth: 0,}]}>
-                        <TouchableOpacity onPress={() => navigation.navigate("ForgotPassword1")}>
-                            <Text style= {{textAlign:'right', textDecorationLine:'underline'}}>Forgot Password?</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style = {{flex: 7}}></View>
+                <TouchableOpacity onPress={() => navigation.navigate("Onboarding")}>
+                    <Icon name="chevron-back-outline" size={20} color='white' />
+                </TouchableOpacity>
+                <View style = {{flex: '35%'}}></View>
+                <View style = {{flex: '20%'}}>
+                    <Text style = {{color: 'white'}}>Logo here</Text>
                 </View>
+                <View style = {{flex: '40%'}}></View>
             </View>
+
+            <View style = {{flex: '5%'}}></View>
+
+            <View style = {{flex: '7%', alignItems: 'center', justifyContent: 'center',}}>
+                <Text style ={{color: 'white', fontSize:35, fontWeight:'bold'}}> Welcome Back</Text>
+            </View>
+
+            <View style = {{flex: '5%',alignItems: 'center', justifyContent: 'center',}}>
+                <Text style = {{fontSize: 14, color: '#79abe0'}}>Let's get you back to the party!</Text>
+            </View>
+
+            <View style = {{flex: '2%'}}></View>
+
+            <View style = {{flex: '30%',backgroundColor:'white', borderWidth:2, borderColor:'white',
+                width:250, }}>
+                <Text>Image here</Text>
+            </View>
+
+            <View style = {{flex: '5%'}}></View>
+
+            <View style = {[styles.container,{flex: '8%',}]}>
+                <TextInput 
+                    style={[styles.input,{color:'grey'}]}
+                    onChangeText={onNumber} 
+                    value={number}
+                    placeholder='Phone Number'
+                    placeholderTextColor={'grey'}
+                />
+            </View>
+
+            <View style = {{flex: '2%'}}></View>
+
+            <View style = {{flex: '2%',width:250}}>
+                <TouchableOpacity onPress={() => navigation.navigate("ForgotPassword1")}>
+                    <Text style= {{textAlign:'right', textDecorationLine:'underline',color:'grey'}}>Forgot Password?</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style = {[styles.container,{flex: '10%',}]}>
+                <TextInput 
+                    style={[styles.input,{color:'grey'}]}
+                    onChangeText={onChangePassword} 
+                    value={password}
+                    placeholder='Password'
+                    placeholderTextColor={'grey'}
+                    secureTextEntry
+                />
+            </View>
+
+            <View style = {{flex: '5%'}}></View>
+
+            <View style = {{flex: '10%', alignItems: 'center',justifyContent: 'center'}}>
+                <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                    <View style = {{backgroundColor: '#79abe0', width:220, height:50, justifyContent:'center', 
+                        alignItems:'center', borderRadius:5}}>
+
+                        <Text style={{color: 'black', fontSize: 14,fontWeight:'bold'}}>SIGN IN</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            <View style = {{flex: '15%'}}></View>
         </View>
     );
 };
@@ -91,13 +103,14 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     input: {
-        height: 40,
+        height: 60,
         width: 300,
         margin: 12,
         borderWidth: 2,
         padding: 10,
         fontSize:14,
-        borderRadius:5,
+        borderRadius:15,
+        borderColor:'grey',
         shadowOffset: {
             width: 5,
             height: 5,
